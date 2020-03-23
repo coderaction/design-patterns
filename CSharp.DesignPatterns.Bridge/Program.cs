1fc9283c19561a545d04a4a6097182791c88540c
serkaneren <serkaneren@outlook.com>
2020-03-23 14:09:49 +0300

    ilk

---
+++ CSharp.DesignPatterns.Bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.DesignPatterns.Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Corona!");
+        }
+    }
+}
